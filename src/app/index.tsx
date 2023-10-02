bd6e3990f2c1f33b3dbe03bcaac802afc6c7d31e
shunwitter <3123900+shunwitter@users.noreply.github.com>
2023-10-02 20:58:17 +0900

    Style circle button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -45,8 +45,8 @@ const Index = (): JSX.Element => {
 
       </View>
 
-      <View>
-        <Text>＋</Text>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>＋</Text>
       </View>
     </View>
   )
@@ -96,6 +96,27 @@ const styles = StyleSheet.create({
     fontSize: 12,
     lineHeight: 16,
     color: '#848484'
+  },
+  circleButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: '#467FD3',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000000',
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 8 },
+    elevation: 8
+  },
+  circleButtonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 48
   }
 })
 
