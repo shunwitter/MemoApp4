1280d470181056c390cbc77c5ab5070160477288
shunwitter <3123900+shunwitter@users.noreply.github.com>
2023-10-04 21:22:37 +0900

    Create log in page

---
+++ src/app/auth/log_in.tsx
@@ -0,0 +1,31 @@
+import { View, Text, TextInput, StyleSheet } from 'react-native'
+
+import Header from '../../components/Header'
+
+const LogIn = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View>
+        <Text>Log In</Text>
+        <TextInput value='Email address' />
+        <TextInput value='Password' />
+        <View>
+          <Text>Submit</Text>
+        </View>
+        <View>
+          <Text>Not registered?</Text>
+          <Text>Sign up here!</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
+
+export default LogIn
