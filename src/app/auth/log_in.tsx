a84c7f9e7ebc5835f4cd502f908861ebf0768a5b
shunwitter <3123900+shunwitter@users.noreply.github.com>
2023-10-06 09:07:31 +0900

    Improve TextInput options

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -23,11 +23,19 @@ const LogIn = (): JSX.Element => {
           style={styles.input}
           value={email}
           onChangeText={(text) => { setEmail(text) }}
+          autoCapitalize='none'
+          keyboardType='email-address'
+          placeholder='Email Address'
+          textContentType='emailAddress'
         />
         <TextInput
           style={styles.input}
           value={password}
           onChangeText={(text) => { setPassword(text) }}
+          autoCapitalize='none'
+          secureTextEntry
+          placeholder='Password'
+          textContentType='password'
         />
         <Button label='Submit' onPress={handlePress} />
         <View style={styles.footer}>
