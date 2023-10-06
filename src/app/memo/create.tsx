def3dd9e1324768641eb4957d41b6b8f16397b0a
shunwitter <3123900+shunwitter@users.noreply.github.com>
2023-10-06 15:56:33 +0900

    Add document to firestore

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -2,12 +2,23 @@ import {
   View, TextInput, StyleSheet, KeyboardAvoidingView
 } from 'react-native'
 import { router } from 'expo-router'
+import { collection, addDoc } from 'firebase/firestore'
 
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
+import { db } from '../../config'
 
 const handlePress = (): void => {
-  router.back()
+  addDoc(collection(db, 'memos'), {
+    bodyText: 'test'
+  })
+    .then((docRef) => {
+      console.log('success', docRef.id)
+      router.back()
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 }
 
 const Create = (): JSX.Element => {
