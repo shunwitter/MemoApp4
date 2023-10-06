f6e0857e4382e66d1eb074160932c4b34e909a6c
shunwitter <3123900+shunwitter@users.noreply.github.com>
2023-10-06 10:33:17 +0900

    Fix constant name EXPO_PUBLIC_FB_MESSAGING_SENDER_ID

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   authDomain: process.env.EXPO_PUBLIC_FB_AUTH_DOMAIN,
   projectId: process.env.EXPO_PUBLIC_FB_PROJECT_ID,
   storageBucket: process.env.EXPO_PUBLIC_FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FB_MESSAGINGSENDER_ID,
+  messagingSenderId: process.env.EXPO_PUBLIC_FB_MESSAGING_SENDER_ID,
   appId: process.env.EXPO_PUBLIC_FB_APP_ID
 }
 
