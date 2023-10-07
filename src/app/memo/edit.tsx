42dddab95590dbb0c5df2915b85a63c88e5e0a6e
shunwitter <3123900+shunwitter@users.noreply.github.com>
2023-10-07 20:49:09 +0900

    Bug fix for edit page

--- src/app/memo/edit.tsx
+++ src/app/memo/edit.tsx
@@ -1,10 +1,11 @@
 import {
-  View, TextInput, StyleSheet, KeyboardAvoidingView, Alert
+  View, TextInput, StyleSheet, Alert
 } from 'react-native'
 import { router, useLocalSearchParams } from 'expo-router'
 import { useState, useEffect } from 'react'
 import { doc, getDoc, setDoc, Timestamp } from 'firebase/firestore'
 
+import KeyboardAvoidingView from '../../components/KeyboardAvoidingView'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
 import { auth, db } from '../../config'
@@ -42,13 +43,14 @@ const Edit = (): JSX.Element => {
   }, [])
 
   return (
-    <KeyboardAvoidingView behavior='height' style={styles.container}>
+    <KeyboardAvoidingView style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
           multiline
           style={styles.input}
           value={bodyText}
           onChangeText={(text) => { setBodyText(text) }}
+          autoFocus
         />
       </View>
       <CircleButton onPress={() => { handlePress(id, bodyText) }}>
